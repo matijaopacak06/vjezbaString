2939bf44ef2cca7a732df6ff55db1645a5dbd7b7
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-01-16 12:08:40 +0100

    Add files via upload

---
+++ vjezbaString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hrvatska ima more";
+            string s2 = "Italija je prvak svijeta";
+            string s3 = s1.Substring(0, 7) + s2.Substring(6);
+            Console.WriteLine(s3);
+            Console.ReadLine();
+        }
+    }
+}
